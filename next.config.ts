500444718164fb408320ef2f481690774ac262eb
mika.arip <mika.arip@quadrant-si.id>
2026-08-14 10:40:53 +0700

    Fixing added new dependencies for vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Next.js 15+
-  serverExternalPackages: ["ghostfetch"],
+  // External packages that should be bundled with the serverless function
+  serverExternalPackages: ["ghostfetch", "@ghostfetch/engine-linux-x64"],
+
+  // Include go engine on vercel
+  outputFileTracingIncludes: {
+    "/api/**/*": [
+      "./node_modules/@ghostfetch/**/*",
+      "./node_modules/ghostfetch/**/*",
+    ],
+  },
 };
 
 export default nextConfig;
